322a8ba0da9696e6182aa766ce80950840b9597f
R. PB Prameshwara Dermawan <pbprameshwara@gmail.com>
2017-11-24 09:11:44 +0700

    Commit Term 1
    Commit kerjaan term

---
+++ src/main/java/com/example/controller/TermController.java
@@ -0,0 +1,136 @@
+package com.example.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import com.example.model.*;
+import com.example.service.*;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+public class TermController
+{
+    @Autowired
+    TermService termDAO;
+
+
+    @RequestMapping("/term/detail")
+    public String view (Model model,
+            @RequestParam(value = "id", required = false) int id)
+    {
+        TermModel term = termDAO.selectTerm (id);
+
+        if (term != null) {
+            model.addAttribute ("term", term);
+            return "term-detail";
+        } else {
+            model.addAttribute ("id", id);
+            return "term-not-found";
+        }
+    }
+
+
+    @RequestMapping("/term/detail/{id}")
+    public String viewPath (Model model,
+            @PathVariable(value = "id") int id)
+    {
+        TermModel term = termDAO.selectTerm(id);
+
+        if (term != null) {
+            model.addAttribute ("term", term);
+            return "term-detail";
+        } else {
+            model.addAttribute ("id", id);
+            return "term-not-found";
+        }
+    }
+
+
+    @RequestMapping("/term")
+    public String view (Model model)
+    {
+        List<TermModel> terms = termDAO.selectAllTerms();
+        model.addAttribute ("terms", terms);
+
+        return "term";
+    }
+
+    
+    @RequestMapping("/term/update/{id}")
+    public String updatePath (Model model,
+                            @PathVariable(value = "id") int id)
+    {
+        TermModel term = termDAO.selectTerm(id);
+
+        if (term != null) {
+            model.addAttribute ("term", term);
+            return "term-update";
+        } else {
+            model.addAttribute ("id", id);
+            return "term-not-found";
+        }
+    }
+
+    @RequestMapping("/term/update/submit")
+    public String updateSubmit (
+            @ModelAttribute TermModel term)
+    {
+        term.updateTermType(term.getTS(), term.getTermType());
+    	termDAO.updateTerm(term);
+        return "term-success-update";
+    }
+
+    
+    @RequestMapping("/term/add/submit")
+    public String addSubmit (
+            @RequestParam(value = "nomor", required = false) String nomor,
+            @RequestParam(value = "tglIrsStart", required = false) String tglIrsStart,
+            @RequestParam(value = "tglIrsEnd", required = false) String tglIrsEnd,
+            @RequestParam(value = "isiIrsStart", required = false) String isiIrsStart,
+            @RequestParam(value = "isiIrsEnd", required = false) String isiIrsEnd,
+            @RequestParam(value = "termString", required = false) String termString)
+    {
+        int termType= 0;
+        if(termString.equals("Ganjil")) {
+        	termType = 1;
+        }
+        else if(termString.equals("Genap")) {
+        	termType = 2;
+        }
+        else {
+        	termType= 3;
+        }
+        	
+        TermModel term = new TermModel (null, nomor, tglIrsStart, tglIrsEnd, isiIrsStart, isiIrsEnd, termType, termString);
+        log.info(""+term);
+    	termDAO.addTerm(term);
+
+        return "term-success-add";
+    }
+
+    
+    @RequestMapping("/term/delete/{id}")
+    public String delete (Model model, @PathVariable(value = "id") int id)
+    {
+    	TermModel term = termDAO.selectTerm (id);
+
+        if (term != null) {
+            termDAO.deleteTerm(id);
+            return "term-delete";
+        } else {
+            model.addAttribute ("id", id);
+            return "term-not-found";
+        }
+        
+    }
+    
+}
---
+++ src/main/java/com/example/dao/TermMapper.java
@@ -0,0 +1,54 @@
+package com.example.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Many;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.Results;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
+import com.example.model.*;
+
+@Mapper
+public interface TermMapper
+{
+    @Select("select id, nomor, tgl_irs_start, tgl_irs_end, isi_irs_start, isi_irs_end, term_type from term where id = #{id}")
+    @Results(value = {
+    		@Result(property="id", column="id"),
+			@Result(property="nomor", column="nama"),
+			@Result(property="tglIrsStart", column="tgl_irs_start"),
+			@Result(property="tglIrsEnd", column="tgl_irs_end"),
+			@Result(property="isiIrsStart", column="isi_irs_start"),
+			@Result(property="isiIrsEnd", column="isi_irs_end"),
+			@Result(property="termType", column="term_type")
+    })
+    TermModel selectTerm (@Param("id") int id);
+
+    @Select("select id, nomor, tgl_irs_start, tgl_irs_end, isi_irs_start, isi_irs_end, term_type from term")
+    @Results(value = {
+			@Result(property="id", column="id"),
+			@Result(property="nomor", column="nama"),
+			@Result(property="tglIrsStart", column="tgl_irs_start"),
+			@Result(property="tglIrsEnd", column="tgl_irs_end"),
+			@Result(property="isiIrsStart", column="isi_irs_start"),
+			@Result(property="isiIrsEnd", column="isi_irs_end"),
+			@Result(property="termType", column="term_type")
+    })
+    List<TermModel> selectAllTerms();
+
+    @Insert("INSERT INTO term (id, nomor, tgl_irs_start, tgl_irs_end, isi_irs_start, isi_irs_end, term_type) VALUES (null, #{nomor}, #{tglIrsStart}, #{tglIrsEnd}, #{isiIrsStart}, #{isiIrsEnd}, #{termType})")
+    void addTerm (TermModel term);
+    
+    @Delete("delete from term where id= #{id}")
+    void deleteTerm (@Param("id") int id);
+
+    @Update("update term set nomor = #{nomor}, tgl_irs_start = #{tglIrsStart}, tgl_irs_end = #{tglIrsEnd}, isi_irs_start = #{isiIrsStart}, isi_irs_end = #{isiIrsEnd}, term_type = #{termType} where id = ${id}")
+    void updateTerm (TermModel term);
+
+    
+}
