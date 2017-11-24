24d49dfdabd7ad9a2b693beaccae33e64747bc84
R. PB Prameshwara Dermawan <pbprameshwara@gmail.com>
2017-11-24 10:46:36 +0700

    Commit API Term 1
    Commit

---
+++ src/main/java/com/example/rest/TermRestController.java
@@ -0,0 +1,30 @@
+package com.example.rest;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.model.TermModel;
+import com.example.service.TermService;
+
+@RestController
+@RequestMapping("/api")
+public class TermRestController {
+	@Autowired
+	TermService termService;
+
+	@RequestMapping("/getTerm/{id}")
+	public TermModel view(@PathVariable(value = "id") int id) {
+		TermModel term = termService.selectTerm(id);
+		return term;
+	}
+	
+	@RequestMapping("/getTerm/")
+	public List<TermModel> view() {
+		List<TermModel> terms = termService.selectAllTerms();
+		return terms;
+	}
+}
