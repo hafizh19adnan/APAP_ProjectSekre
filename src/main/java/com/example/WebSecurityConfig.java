2ee2c6fb99e617f6cd2d1cb31ab54029c14528a3
hafizh19adnan <hafizh19adnan@gmail.com>
2017-12-03 19:14:48 +0700

    Update frontend

---
+++ src/main/java/com/example/WebSecurityConfig.java
@@ -0,0 +1,33 @@
+//package com.example;
+//
+//import javax.activation.DataSource;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//
+//@Configuration
+//@EnableWebSecurity
+//public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
+//	@Override
+//	protected void configure(HttpSecurity http) throws Exception{
+//		http.authorizeRequests().
+//		antMatchers("/").permitAll().anyRequest().authenticated().and().formLogin().
+//		loginPage("/").permitAll().and().logout().permitAll();
+//	}
+//	@Autowired
+//	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception{
+//		auth.inMemoryAuthentication().withUser("user").password("password").roles("USER");
+//		auth.inMemoryAuthentication().withUser("admin").password("admin").roles("ADMIN");
+//	}
+////	@Autowired
+////	javax.sql.DataSource dataSource;
+////
+////	@Autowired
+////	public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception{
+////		auth.jdbcAuthentication().dataSource(dataSource).usersByUsernameQuery("select username,password,enabled from users where username =? "). authoritiesByUsernameQuery( "select username, role from user_roles where username =? ");		
+////	}
+//}
