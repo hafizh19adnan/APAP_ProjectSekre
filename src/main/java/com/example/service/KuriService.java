5eae4ea56bacbb2cef9e998cf34cfea17fd82431
sandarklinton <sandarklinton@gmail.com>
2017-12-06 18:39:10 +0700

    kuriservice+serviceDB

--- src/main/java/com/example/service/KuriService.java
+++ src/main/java/com/example/service/KuriService.java
@@ -1,5 +1,14 @@
 package com.example.service;
 
-public class KuriService {
+import java.util.List;
 
+import com.example.model.KurikulumModel;
+import com.example.model.MahasiswaModel;
+
+public interface KuriService {
+
+	List<KurikulumModel> allKurikulum();
+	List<MahasiswaModel> allMahasiswa();
+	List<String> allAngkatan(List<MahasiswaModel> listMahasiswa);
+	
 }
--- src/main/java/com/example/service/KuriServiceDatabase.java
+++ src/main/java/com/example/service/KuriServiceDatabase.java
@@ -1,5 +1,40 @@
 package com.example.service;
 
-public class KuriServiceDatabase {
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.dao.AppMapper;
+import com.example.dao.KuriMapper;
+import com.example.model.KurikulumModel;
+import com.example.model.MahasiswaModel;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
+public class KuriServiceDatabase implements KuriService{
+
+//	@Autowired
+//    private KuriMapper kuriMapper;
+	
+	@Override
+	public List<KurikulumModel> allKurikulum() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<MahasiswaModel> allMahasiswa() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<String> allAngkatan(List<MahasiswaModel> listMahasiswa) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
